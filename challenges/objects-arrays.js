54493332bd43d171e9c272c91fe0ddeac2c25cd6
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-25 09:48:45 -0700

    Added contact info array and unit array.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,7 +76,7 @@ Once you have the new array created, sort the universities alphabetically and lo
 const universities = [];
 for(let i = 0; i < graduates.length; i++) {
   universities.push(graduates[i].university);
-}
+};
 universities.sort();
 console.log(universities)
 
@@ -87,11 +87,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+};
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for(let i = 0; i < graduates.length; i++) {
+  if(graduates[i].university.includes("Uni")) {
+    uni.push(graduates[i].university);
+  }
+};
 console.log(uni);
 
 
